07a4eb2ec3b7f0f611441147cd3464a83d089ae7
kevdpow <kevdpow@gmail.com>
2022-08-14 15:25:23 -0400

    add redis

--- genz_tweet.py
+++ genz_tweet.py
@@ -1,10 +1,14 @@
 from asyncore import read
 import tweepy
 import time
+import redis
 import os
 from pprint import pprint
 import random
 from generate_nouns import get_nouns
+import datetime
+
+date_pattern = '%Y-%m-%d %H:%M:%S.%f'
 
 INTERVAL = 60 * 30
 
@@ -20,39 +24,47 @@ client = tweepy.Client(
 )
 
 
-# def read_used_nouns():
-#     with open("usedNouns.log", mode="r", encoding="utf-8") as outfile:
-#         data = [r.replace("\n", "") for r in outfile.readlines()]
-#         return data
+def get_used_nouns():
+    r = redis.from_url(os.environ.get("REDIS_URL"))
+    usedNouns = r.lrange('used_nouns', 0, -1)
+    start = r.get("start")
+    if start:
+        startObj = datetime.datetime.strptime(
+            start.decode('utf-8'), date_pattern)
+        endObj = startObj + datetime.timedelta(days=30)
+        if datetime.datetime.now() > endObj:
+            r.delete("used_nouns")
+            r.set("start", str(datetime.datetime.now()))
+            return []
+    elif not start:
+        r.set("start", str(datetime.datetime.now()))
+    if not usedNouns:
+        return []
+    return [n.decode("utf-8") for n in usedNouns]
 
 
 def write_used_noun(noun):
-    str = noun + "\n"
-    with open("usedNouns.log", mode="a", encoding='utf-8') as infile:
-        infile.write(str)
+    r = redis.from_url(os.environ.get("REDIS_URL"))
+    p = r.lpush("used_nouns", noun)
+    return p
 
 
 def send_genz_tweet():
-    # usedNouns = read_used_nouns()
-    nouns = get_nouns()
-    if len(nouns):
+    usedNouns = get_used_nouns()
+    availableNouns = get_nouns()
+    print(usedNouns)
+    if len(availableNouns):
+        nouns = [noun for noun in availableNouns if noun
+                 not in usedNouns]
         noun = random.choice(nouns)
-        tweet = "does gen z know about {}".format(noun)
-        client.create_tweet(text=tweet)
-    # if len(availableNouns):
-    #     nouns = [noun for noun in availableNouns if noun
-    #              not in usedNouns]
-    #     noun = random.choice(nouns)
-    #     write_used_noun(noun)
-    # else:
-    #     nouns = usedNouns
-    #     noun = random.choice(nouns)
-    # tweet = "does gen z know about {}".format(noun)
-    # client.create_tweet(text=tweet)
+        write_used_noun(noun)
+    else:
+        nouns = usedNouns
+        noun = random.choice(nouns)
+    tweet = "does gen z know about {}".format(noun)
+    client.create_tweet(text=tweet)
 
 
-send_genz_tweet()
-
-# while True:
-#     send_genz_tweet()
-#     time.sleep(INTERVAL)
+while True:
+    send_genz_tweet()
+    time.sleep(INTERVAL)
