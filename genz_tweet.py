0a3b777c53c19bc9b4cb34429516aa8860a1f756
kevdpow <kevdpow@gmail.com>
2022-08-18 01:41:06 -0400

    add redis

--- genz_tweet.py
+++ genz_tweet.py
@@ -7,13 +7,10 @@ from pprint import pprint
 import random
 from generate_nouns import get_nouns
 import datetime
+import redis
 
 date_pattern = '%Y-%m-%d %H:%M:%S.%f'
-# r = redis.from_url(os.environ.get("REDIS_URL"))
-
-BOT_INTERVAL = 60 * 28.5
-
-# Authenticate to Twitter
+r = redis.from_url(os.environ.get("REDIS_URL"))
 
 creds = {"consumer_key": os.environ.get("TWITTER_API_KEY"),
          "consumer_secret": os.environ.get("TWITTER_API_SECRET"),
@@ -25,41 +22,38 @@ client = tweepy.Client(
 )
 
 
-# def get_used_nouns():
-#     usedNouns = r.lrange('used_nouns', 0, -1)
-#     start = r.get("start")
-#     if start:
-#         startObj = datetime.datetime.strptime(
-#             start.decode('utf-8'), date_pattern)
-#         endObj = startObj + datetime.timedelta(days=30)
-#         if datetime.datetime.now() > endObj:
-#             r.delete("used_nouns")
-#             r.set("start", str(datetime.datetime.now()))
-#             return []
-#     elif not start:
-#         r.set("start", str(datetime.datetime.now()))
-#     if not usedNouns:
-#         return []
-#     return [n.decode("utf-8") for n in usedNouns]
+def get_used_nouns():
+    usedNouns = r.lrange('used_nouns', 0, -1)
+    start = r.get("start")
+    if start:
+        startObj = datetime.datetime.strptime(
+            start.decode('utf-8'), date_pattern)
+        endObj = startObj + datetime.timedelta(days=30)
+        if datetime.datetime.now() > endObj:
+            r.delete("used_nouns")
+            r.set("start", str(datetime.datetime.now()))
+            return []
+    elif not start:
+        r.set("start", str(datetime.datetime.now()))
+    if not usedNouns:
+        return []
+    return [n.decode("utf-8") for n in usedNouns]
 
 
 def send_genz_tweet():
-    # usedNouns = get_used_nouns()
-    nouns = get_nouns()
-    if len(nouns):
+    usedNouns = get_used_nouns()
+    availableNouns = get_nouns()
+    if len(availableNouns):
+        nouns = [noun for noun in availableNouns if noun
+                 not in usedNouns]
         noun = random.choice(nouns)
-        tweet = "does gen z know about {}".format(noun)
-        print(str(datetime.datetime.now()), tweet)
-        client.create_tweet(text=tweet)
-        return
-    # if len(availableNouns):
-    #     nouns = [noun for noun in availableNouns if noun
-    #              not in usedNouns]
-    #     noun = random.choice(nouns)
-    #     # r.lpush("used_nouns", noun)
-    # else:
-    #     nouns = usedNouns
-    #     noun = random.choice(nouns)
+        r.lpush("used_nouns", noun)
+    else:
+        nouns = usedNouns
+        noun = random.choice(nouns)
+    tweet = "does gen z know about {}".format(noun)
+    print(str(datetime.datetime.now()), tweet)
+    client.create_tweet(text=tweet)
 
 
 if __name__ == '__main__':
